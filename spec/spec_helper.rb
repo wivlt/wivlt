31671128e5eeba9e6d096f043ab18f56b301267a
Haroldo Furtado <haroldofurtado@gmail.com>
2025-04-23 09:58:43 -0300

    feat: add documentation formatter for individual spec file runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -51,6 +51,16 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = :random
 
+  #   # Many RSpec users commonly either run the entire suite or an individual
+  #   # file, and it's useful to allow more verbose output when running an
+  #   # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  end
+
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
   #   # This allows you to limit a spec run to individual examples or groups
@@ -73,16 +83,6 @@ RSpec.configure do |config|
   #   # This setting enables warnings. It's recommended, but in some cases may
   #   # be too noisy due to issues in dependencies.
   #   config.warnings = true
-  #
-  #   # Many RSpec users commonly either run the entire suite or an individual
-  #   # file, and it's useful to allow more verbose output when running an
-  #   # individual spec file.
-  #   if config.files_to_run.one?
-  #     # Use the documentation formatter for detailed output,
-  #     # unless a formatter has already been configured
-  #     # (e.g. via a command-line flag).
-  #     config.default_formatter = "doc"
-  #   end
   #
   #   # Print the 10 slowest examples and example groups at the
   #   # end of the spec run, to help surface which specs are running
